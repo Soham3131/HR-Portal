65d399b2ae4f8a51ebb5076362855793cc78339c
Soham3131 <sohamdang0@gmail.com>
2025-07-21 16:01:25 +0530

    Added app.js

--- hrportal/src/App.js
+++ hrportal/src/App.js
@@ -6,18 +6,32 @@ import AppRouter from './routes/AppRouter';
 import Layout from './components/Layout';
 import { ThemeProvider } from "./context/ThemeContext"
 
+// function App() {
+//   return (
+//     <AuthProvider>
+//       <Router>
+//         <Layout>
+//              <ThemeProvider attribute="class"><AppRouter /></ThemeProvider>
+          
+          
+//         </Layout>
+//       </Router>
+//     </AuthProvider>
+//   );
+// }
+
+// export default App;
+
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Layout>
-             <ThemeProvider attribute="class"><AppRouter /></ThemeProvider>
-          
-          
-        </Layout>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <ThemeProvider attribute="class">
+          <Layout>
+            <AppRouter />
+          </Layout>
+        </ThemeProvider>
+      </AuthProvider>
+    </Router>
   );
 }
-
-export default App;
