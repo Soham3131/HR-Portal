bacb858c479d34ab463539023d2155b8cb264fdd
soham313131 <sohamdang313131@gmail.com>
2025-09-14 11:59:59 +0530

    chnged model

--- hrportal/server/models/Employee.js
+++ hrportal/server/models/Employee.js
@@ -1,4 +1,4 @@
-// // models/Employee.js
+
 // const mongoose = require('mongoose');
 // const bcrypt = require('bcryptjs');
 
@@ -39,30 +39,56 @@
 //     dob: {
 //         type: Date,
 //     },
-//     joiningDate: { // New Field
+//     joiningDate: {
 //         type: Date,
 //     },
-//     salary: { // New Field
+//     salary: {
 //         type: Number,
 //         default: 0,
 //     },
-//     profilePictureUrl: { // New Field
+//     profilePictureUrl: {
 //         type: String,
 //         default: '',
 //     },
-//     documents: [documentSchema], // New Field for marksheets
-//     idProofUrl: { // New Field
+//     documents: [documentSchema],
+//     idProofUrl: {
 //         type: String,
 //         default: '',
+//     },
+//     isVerified: {
+//         type: Boolean,
+//         default: false,
+//     },
+//     otp: {
+//         type: String,
+//     },
+
+
+//     otpExpires: {
+//         type: Date,
 //     },
 //     holidaysLeft: {
 //         type: Number,
 //         default: 2,
 //     },
+//     status: {
+//         type: String,
+//         enum: ['Active', 'Deactivated'],
+//         default: 'Active',
+//     },
+//     deactivationDate: {
+//         type: Date,
+//     },
+
+//     readAnnouncements: [{
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Announcement'
+//     }],
 // }, { timestamps: true });
 
-// // Pre-save hook for hashing password and generating employee ID
+// // This pre-save hook now ONLY handles the password and employee ID
 // EmployeeSchema.pre('save', async function(next) {
+//     // Generate Employee ID only if it's a new employee
 //     if (this.isNew && !this.employeeId) {
 //         const lastEmployee = await this.constructor.findOne({}, {}, { sort: { 'createdAt' : -1 } });
 //         let newIdNumber = 1001;
@@ -73,6 +99,8 @@
 //         this.employeeId = `AVANI-${newIdNumber}`;
 //     }
 
+//     // Hash the password ONLY if it has been modified (or is new).
+//     // This will no longer affect the OTP.
 //     if (!this.isModified('password')) {
 //         return next();
 //     }
@@ -88,7 +116,6 @@
 // const Employee = mongoose.model('Employee', EmployeeSchema);
 // module.exports = Employee;
 
-// models/Employee.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
@@ -159,36 +186,73 @@ const EmployeeSchema = new mongoose.Schema({
         type: Number,
         default: 2,
     },
+    status: {
+        type: String,
+        enum: ['Active', 'Deactivated'],
+        default: 'Active',
+    },
+    deactivationDate: {
+        type: Date,
+    },
     readAnnouncements: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Announcement'
     }],
 }, { timestamps: true });
 
-// This pre-save hook now ONLY handles the password and employee ID
-EmployeeSchema.pre('save', async function(next) {
-    // Generate Employee ID only if it's a new employee
-    if (this.isNew && !this.employeeId) {
-        const lastEmployee = await this.constructor.findOne({}, {}, { sort: { 'createdAt' : -1 } });
-        let newIdNumber = 1001;
-        if (lastEmployee && lastEmployee.employeeId) {
-            const lastIdNumber = parseInt(lastEmployee.employeeId.split('-')[1]);
-            newIdNumber = lastIdNumber + 1;
-        }
-        this.employeeId = `AVANI-${newIdNumber}`;
-    }
 
-    // Hash the password ONLY if it has been modified (or is new).
-    // This will no longer affect the OTP.
-    if (!this.isModified('password')) {
-        return next();
+// --- Pre-save hook ---
+EmployeeSchema.pre('save', async function (next) {
+    try {
+        // Generate Employee ID only if it's a new employee
+        if (this.isNew && !this.employeeId) {
+            const lastEmployee = await this.constructor.findOne({}, {}, { sort: { 'createdAt': -1 } });
+            let newIdNumber = 1001;
+            if (lastEmployee && lastEmployee.employeeId) {
+                const lastIdNumber = parseInt(lastEmployee.employeeId.split('-')[1]);
+                newIdNumber = lastIdNumber + 1;
+            }
+            this.employeeId = `AVANI-${newIdNumber}`;
+        }
+
+        // Hash the password ONLY if it has been modified (or is new).
+        if (!this.isModified('password')) {
+            return next();
+        }
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
     }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
-EmployeeSchema.methods.matchPassword = async function(enteredPassword) {
+
+// --- Handle duplicate key error (retry logic) ---
+EmployeeSchema.post('save', async function (error, doc, next) {
+    if (error && error.name === 'MongoServerError' && error.code === 11000 && error.keyPattern.employeeId) {
+        // Retry logic for duplicate employeeId
+        try {
+            const lastEmployee = await this.constructor.findOne({}, {}, { sort: { 'createdAt': -1 } });
+            let newIdNumber = 1001;
+            if (lastEmployee && lastEmployee.employeeId) {
+                const lastIdNumber = parseInt(lastEmployee.employeeId.split('-')[1]);
+                newIdNumber = lastIdNumber + 1;
+            }
+            this.employeeId = `AVANI-${newIdNumber}`;
+            await this.save(); // retry save
+            next();
+        } catch (retryErr) {
+            next(retryErr);
+        }
+    } else {
+        next(error);
+    }
+});
+
+
+// --- Password compare method ---
+EmployeeSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
